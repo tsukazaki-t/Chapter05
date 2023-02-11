87355cc7a658d2d5e74ff8c5e03b25f4de6cdaa8
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-11 23:12:08 +0900

    課題提出

---
+++ src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//String型の変数宣言
+		String words;
+		
+		//wordsを自分の名前に初期化および出力
+		words = "Tomomi Tsukazaki";
+		System.out.println(words);
+		
+		//wordsを自分の好きなことばに初期化および出力
+		words = "いつも心にナイフを";
+		System.out.println(words);
+		
+		//「int」型のfinal定数を宣言し、ラッキーナンバーを代入
+		final int luckyNum = 7;
+		System.out.println(luckyNum);
+	}
+
+}
